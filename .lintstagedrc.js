c9f336af2e81a529034ca87c1da765aafa16be93
Tomás Keong <tomaskeong@gmail.com>
2022-12-03 21:18:06 +0000

    test: downgrade husky

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,11 +3,17 @@ const path = require('path');
 module.exports = {
   // Lint & Prettify TS and JS files
   '**/*.(ts|tsx|js)': (filenames) => [
-    `yarn eslint ${filenames.join(' ')}`,
-    `yarn prettier --write ${filenames.join(' ')}`,
+    `next lint --fix --file ${filenames
+      .map((f) => path.relative(process.cwd(), f))
+      .join(' --file ')}`,
+    `yarn prettier --write ${filenames
+      .map((f) => path.relative(process.cwd(), f))
+      .join(' --file ')}`,
   ],
 
   // Prettify only Markdown and JSON files
   '**/*.(md|json)': (filenames) =>
-    `yarn prettier --write ${filenames.join(' ')}`,
+    `yarn prettier --write ${filenames
+      .map((f) => path.relative(process.cwd(), f))
+      .join(' --file ')}`,
 };
