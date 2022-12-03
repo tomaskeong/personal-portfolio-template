9f739b30a2132a12f359c76be4fd01c016b82a32
Tomás Keong <tomaskeong@gmail.com>
2022-12-03 21:05:54 +0000

    feat: add lintstaged commands

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,10 +1,13 @@
-const path = require('path');
-
-const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(' --file ')}`;
-
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  // Lint & Prettify TS and JS files
+  '**/*.(ts|tsx|js)': (filenames) => [
+    `next lint --fix --file ${filenames
+      .map((f) => path.relative(process.cwd(), f))
+      .join(' --file ')}`,
+    `yarn prettier --write ${filenames.join(' ')}`,
+  ],
+
+  // Prettify only Markdown and JSON files
+  '**/*.(md|json)': (filenames) =>
+    `yarn prettier --write ${filenames.join(' ')}`,
 };
