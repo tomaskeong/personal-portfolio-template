5f8c9f4863af0506ac6b8550b3dab04fd7385413
Tomás Keong <tomaskeong@gmail.com>
2022-12-03 22:35:58 +0000

    test: lint-staged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,9 +1,12 @@
 const path = require('path');
 
 module.exports = {
+  // Type check TypeScript files
+  '**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
+
   // Lint & Prettify TS and JS files
   '**/*.(ts|tsx|js)': (filenames) => [
-    `next lint --fix --file ${filenames
+    `yarn lint --fix --file ${filenames
       .map((f) => path.relative(process.cwd(), f))
       .join(' --file ')}`,
   ],
