857bd3460519a26b947f83832d2ea9932f2904f9
Tomás Keong <tomaskeong@gmail.com>
2022-12-03 22:29:05 +0000

    test: lint-staged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,10 +1,16 @@
-const path = require('path')
-
-const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(' --file ')}`
+const path = require('path');
 
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
-}
+  // Lint & Prettify TS and JS files
+  '**/*.(ts|tsx|js)': (filenames) => [
+    `next lint --fix --file ${filenames
+      .map((f) => path.relative(process.cwd(), f))
+      .join(' --file ')}`,
+  ],
+
+  // Prettify only Markdown and JSON files
+  '**/*.(md|json)': (filenames) =>
+    `yarn prettier --write ${filenames
+      .map((f) => path.relative(process.cwd(), f))
+      .join(' --file ')}`,
+};
