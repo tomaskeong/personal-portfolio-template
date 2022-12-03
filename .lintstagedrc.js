5df52b7526587fcf03513747f1ff35ae953c7d10
Tomás Keong <tomaskeong@gmail.com>
2022-12-03 21:10:56 +0000

    test: lint staged commands

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,13 +1,19 @@
+const path = require('path');
+
 module.exports = {
   // Lint & Prettify TS and JS files
   '**/*.(ts|tsx|js)': (filenames) => [
     `next lint --fix --file ${filenames
       .map((f) => path.relative(process.cwd(), f))
       .join(' --file ')}`,
-    `yarn prettier --write ${filenames.join(' ')}`,
+    `yarn prettier --write ${filenames
+      .map((f) => path.relative(process.cwd(), f))
+      .join(' --file ')}`,
   ],
 
   // Prettify only Markdown and JSON files
   '**/*.(md|json)': (filenames) =>
-    `yarn prettier --write ${filenames.join(' ')}`,
+    `yarn prettier --write ${filenames
+      .map((f) => path.relative(process.cwd(), f))
+      .join(' --file ')}`,
 };
